893009f2ec8bd53261073faf315728ab208ed029
Devin Frazier <Dev.fraz18@gmail.com>
2025-04-01 15:39:58 -0400

    Each activity links to its own page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
 import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout.jsx";
+import ActivityDetails from "./activities/ActivityDetails.jsx";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -16,6 +17,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route index element={<ActivitiesPage />} />
         <Route path="/activities" element={<ActivitiesPage />} />
+        <Route path="/activities/:id" element={<ActivityDetails />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Error404 />} />
